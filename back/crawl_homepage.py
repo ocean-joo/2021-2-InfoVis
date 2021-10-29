eaf1b86fffccbab19831e58e30e39966af92f93f
Haejoo Jeon <joo.in.ocean@gmail.com>
2021-10-29 11:34:09 +0900

    Extend code to get email information(SNU) and change code structure

--- back/crawl_homepage.py
+++ back/crawl_homepage.py
@@ -3,40 +3,118 @@ from selenium import webdriver
 import time
 
 SCROLL_PAUSE_SEC = 2
+CRAWL = 'snu' # snu, postech, kaist, yonsei, korea
 
-AI_url_list = {'SNU': "https://gsai.snu.ac.kr/ko/portfolio",
-                }
+url_list = {'postech': "http://ai.postech.ac.kr/sub020101",
+                'snu': "https://gsai.snu.ac.kr/ko/portfolio",
+                'kaist':"https://gsai.kaist.ac.kr/?lang=ko",
+                'yonsei':"",
+                'korea':""}
 
-output_list = {'SNU': "output/snu_homepage.csv",
-                }
+output_list = {'postech': "output/postech_homepage.csv",
+                'snu': "output/snu_homepage.csv",
+                'kaist':"output/kaist_homepage.csv",
+                'yonsei':"output/yousei_homepage.csv",
+                'korea':"output/korea_homepage.csv"}
 
-driver = webdriver.Chrome(executable_path='chromedriver')
-driver.get(url=AI_url_list['SNU'])
-f = open(output_list['SNU'], 'w')
+def snu_crawl(path, output) :
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver.get(url=path)
+    f = open(output, 'w')
 
-# Scroll down to bottom
-last_height = driver.execute_script("return document.body.scrollHeight")
+    # Scroll down to bottom
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(SCROLL_PAUSE_SEC)
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_SEC)
 
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-profs_element = driver.find_element_by_css_selector("#lc_content")
+    profs_element = driver.find_element_by_css_selector("#lc_content")
 
-for prof in profs_element.find_elements_by_class_name("d0") :
-    name = prof.find_element_by_css_selector("dl > dt > a").text
-    try :
-        homepage = prof.find_element_by_css_selector("dl > dd > ul > li.l4 > a").text
-    except :
-        homepage = ""
+    for prof in profs_element.find_elements_by_class_name("d0") :
+        name = prof.find_element_by_css_selector("dl > dt > a").text
+        email = prof.find_element_by_css_selector("dl > dd > ul > li.l3 > a").text
+        try :
+            homepage = prof.find_element_by_css_selector("dl > dd > ul > li.l4 > a").text
+        except :
+            homepage = ""
 
-    print("[Crawled]:", name, homepage)
-    f.write("%s,%s\n" % ( name, homepage))
+        print("[Crawled]:", name, email, homepage)
+        f.write("%s,%s,%s\n" % ( name, email, homepage))
 
-driver.close()
-f.close()
+    driver.close()
+    f.close()
+
+def postech_crawl(path, output) :
+    PROXY = "117.102.78.42:8080"
+    webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
+        "httpProxy": PROXY,
+        "ftpProxy": PROXY,
+        "sslProxy": PROXY,
+        "proxyType": "MANUAL",
+    }
+
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver.get(url=path)
+
+    f = open(output, 'w')
+
+    profs_section = driver.find_elements_by_class_name("new-list3")
+
+    for profs_element in profs_section.find_elements_by_css_selector("li") :
+        for prof in profs_element.find_elements_by_css_selector("d0") :
+            name = prof.find_element_by_css_selector("div.list-txt > strong").text
+            try :
+                homepage = prof.find_element_by_css_selector("div.list-txt > strong > img.homepage_class").get_attribute("href")
+            except :
+                homepage = ""
+
+            print("[Crawled]:", name, homepage)
+            f.write("%s,%s\n" % ( name, homepage))
+
+    driver.close()
+    f.close()
+
+def kaist_crawl(path, output) :
+    
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver.get(url=path)
+    driver.implicitly_wait(3)
+    f = open(output, 'w')
+
+    # profs_section = driver.find_element_by_class_name("wpb_wrapper")
+
+    # for prof in profs_section.find_elements_by_class_name("vc_column-inner") :
+    #     name = prof.find_element_by_css_selector("div.wpb_text_column.wpb_content_element > div > p > span > a > strong").text
+    #     try :
+    #         homepage = prof.find_element_by_css_selector("div.wpb_text_column.wpb_content_element > div > p > span > a").get_attribute("href")
+    #     except :
+    #         homepage = ""
+
+    #     print("[Crawled]:", name, homepage)
+    #     f.write("%s,%s\n" % ( name, homepage))
+
+    driver.close()
+    f.close()
+
+def yonsei_crawl(path, output) :
+    pass
+
+def korea_crawl(path, output) :
+    pass
+
+
+func_list = {'postech': postech_crawl,
+                'snu': snu_crawl,
+                'kaist':kaist_crawl,
+                'yonsei':yonsei_crawl,
+                'korea':korea_crawl}
+
+if __name__ == '__main__' :
+    func = func_list[CRAWL]
+    func(url_list[CRAWL], output_list[CRAWL])
