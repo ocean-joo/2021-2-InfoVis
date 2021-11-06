a90c99c22f059222a9d90ac44be09de6b5d08e5c
Spiraline <snowbaepbird@gmail.com>
2021-11-06 19:45:35 +0900

    Add argparse to crawl_paper_bs

--- back/crawl_paper_bs.py
+++ back/crawl_paper_bs.py
@@ -1,8 +1,8 @@
 import bs4, time
 import requests
 from requests.auth import HTTPProxyAuth
-
-CRAWL_SCHOOL = 'postech' # snu, postech, kaist, yonsei, korea
+import random
+import argparse
 
 def crawl(email, output) :
     header =  {'User-Agent': 'Mozilla/5.0'}
@@ -26,7 +26,7 @@ def crawl(email, output) :
         print("[*] No Paper for %s\n" % email)
         return
 
-    time.sleep(5)
+    time.sleep(random.randint(3, 8))
 
     for i in range(0, int((paper_num)/10)+1) :
         if i == 0 :
@@ -37,7 +37,7 @@ def crawl(email, output) :
         sub_result = requests.get(sub_url, headers=header)
         sub_soup = bs4.BeautifulSoup(sub_result.content, 'html.parser')
 
-        time.sleep(3)
+        time.sleep(random.randint(3, 8))
 
         if len(sub_soup.select("#gs_res_ccl_mid > div")) == 0 :
             print("MAYBE BANNED")
@@ -79,15 +79,24 @@ if __name__ == '__main__' :
                         'yonsei':"output/yousei_homepage.csv",
                         'korea':"output/korea_homepage.csv"}
 
-    meta = meta_path_dict[CRAWL_SCHOOL]
+    parser = argparse.ArgumentParser(description='Conference Crawler Argparser')
+    parser.add_argument('--school', '-s', required=True, type=str, help='school')
+    parser.add_argument('--banned', '-b', type=int, default=0, help='banned point')
+    args = parser.parse_args()
+
+    if args.school not in meta_path_dict:
+        exit(1)
+
+    meta = meta_path_dict[args.school]
     
-    with open(meta, 'r') as f : 
+    with open(meta, 'r') as f :
+
         lines = f.readlines()
         for i, line in enumerate(lines) :
             # Start crawling from banned point
-            if i<10 :
+            if i < args.banned :
                 continue
             name, email, _ = line.split(",")
-            path = "output/{}_title_{}.csv".format(CRAWL_SCHOOL, name)
+            path = "output/{}_title_{}.csv".format(args.school, name)
 
             crawl(email, path) 
