b431380303dda2cdf348f801f16ea30cd8596f54
Spiraline <snowbaepbird@gmail.com>
2021-11-08 12:56:26 +0900

    Add prototype metric json

--- back/weight_gen_v1.py
+++ back/weight_gen_v1.py
@@ -1,13 +1,13 @@
 import csv
 import os
-import warnings
+import json
 
 conf_list = []
 lab_list = {}
 adj_matrix = []
+school_list = ["snu", "kaist", "postech", "yonsei", "korea"]
 
 if __name__ == '__main__' :
-    warnings.filterwarnings('ignore')
 
     for conf_file in os.listdir('output/conf_csv'):
         if len(conf_file.split('.')) == 1 or conf_file.split('.')[1] != "csv":
@@ -40,8 +40,9 @@ if __name__ == '__main__' :
                     if conf_in_paper in conf or conf in conf_in_paper:
                         conf_num_list[idx] += 1
 
+            school_name = bibtex_file.split('_')[0]
             prof_name = bibtex_file.split('_')[-1].split('.')[0]
-            lab_list[prof_name] = {"conf_num_list" : conf_num_list, "total_paper_num" : total_paper_num}
+            lab_list[prof_name] = {"conf_num_list" : conf_num_list, "total_paper_num" : total_paper_num, "school_name" : school_name}
     
     for lab_a in lab_list:
         lab_a_adj = [0 for _ in range(len(lab_list))]
@@ -54,4 +55,33 @@ if __name__ == '__main__' :
         
         adj_matrix.append(lab_a_adj)
 
-    print(adj_matrix)
+    ### json encoding
+    json_dict = {}
+    nodes_list = []
+    links_list = []
+
+    for (lab_idx, prof_name) in enumerate(lab_list):
+        new_node = {}
+        new_node["id"] = str(lab_idx)
+        new_node["name"] = prof_name
+        new_node["scale"] = lab_list[prof_name]["total_paper_num"]
+        new_node["cluster"] = school_list.index(lab_list[prof_name]["school_name"])
+        nodes_list.append(new_node)
+
+        for (a_b_adj_idx, a_b_adj) in enumerate(adj_matrix[lab_idx]):
+            if a_b_adj_idx >= lab_idx:
+                continue
+            if a_b_adj > 0:
+                new_link = {}
+                new_link["id"] = str(lab_idx) + "-" + str(a_b_adj_idx)
+                new_link["source"] = lab_idx
+                new_link["target"] = a_b_adj_idx
+                # TODO : scale weight
+                new_link["weight"] = a_b_adj * 100
+                links_list.append(new_link)
+    
+    json_dict["nodes"] = nodes_list
+    json_dict["links"] = links_list
+
+    with open('output/json/weight.json', 'w') as json_file:
+        json.dump(json_dict, json_file)
