15dacc9281c15f79bd2cd628ad64b467141ebddd
Spiraline <snowbaepbird@gmail.com>
2021-11-16 19:16:03 +0900

    Remove value attribute

--- front/community_graph/src/components/ControlPanel.js
+++ front/community_graph/src/components/ControlPanel.js
@@ -6,25 +6,24 @@ class CustomizedRange extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lowerBound: 20,
-            upperBound: 40,
-            value: [20, 40],
+            lowerBound: 0,
+            upperBound: 100
         };
     }
 
     onLowerBoundChange = (e) => {
-        if (+e.target.value < 0 || +e.target.value > this.state.value[1]) return
+        if (+e.target.value < 0 || +e.target.value > this.state.upperBound) return
         this.setState({
             lowerBound: +e.target.value,
-            value: [+e.target.value, this.state.value[1]]
+            value: [+e.target.value, this.state.upperBound]
         });
     };
 
     onUpperBoundChange = (e) => {
-        if (+e.target.value > 100 || +e.target.value < this.state.value[0]) return
+        if (+e.target.value > 100 || +e.target.value < this.state.lowerBound) return
         this.setState({
             upperBound: +e.target.value,
-            value: [this.state.value[0], +e.target.value]
+            value: [this.state.lowerBound, +e.target.value]
         });
     };
 
@@ -32,7 +31,6 @@ class CustomizedRange extends React.Component {
         this.setState({
             lowerBound: value[0],
             upperBound: value[1],
-            value: value,
         });
     };
 
@@ -41,12 +39,20 @@ class CustomizedRange extends React.Component {
             <div>
                 <label>LowerBound: </label>
                 <input type="number" value={this.state.lowerBound} onChange={this.onLowerBoundChange} />
+                <label> %</label>
                 <br />
                 <label>UpperBound: </label>
                 <input type="number" value={this.state.upperBound} onChange={this.onUpperBoundChange} />
+                <label> %</label>
                 <br />
                 <br />
-                <Range allowCross={false} value={this.state.value} onChange={this.onSliderChange} />
+                <Range
+                    allowCross={false}
+                    min={0}
+                    max={100}
+                    value={[this.state.lowerBound, this.state.upperBound]}
+                    onChange={this.onSliderChange}
+                />
             </div>
         );
     }
